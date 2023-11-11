108a66d34b50c385956a4180b4e7b06927e7bbd3
Yashkayadinge <113251763+Yashkayadinge@users.noreply.github.com>
2023-11-11 10:01:43 +0000

    about typeof and container

---
+++ javascript/001/contain.js
@@ -0,0 +1,15 @@
+// console.log("hell0");
+// let il=1;
+// let b=Boolean(il);
+// console.log(il);
+
+
+// //boolean 1 is true ;0 is false
+//"" is false
+// "yash" is true
+
+
+let number = 33;
+let b = String(33)
+console.log(b);
+console.log(typeof b)
