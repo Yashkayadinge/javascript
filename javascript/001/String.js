86ef9426bb7d66cf5bddfbe7351cc068b6511841
Yashkayadinge <113251763+Yashkayadinge@users.noreply.github.com>
2023-11-13 10:02:15 +0000

    Srring_objects

---
+++ javascript/001/String.js
@@ -0,0 +1,28 @@
+// console.log("hello")
+// console.log('yash is a bad person')
+// const name = "yash";
+// const repocount = 40;
+// console.log(`my name is ${name} and my repocount is${repocount}`)
+ const y= new String("yash-k");
+// console.log(y[0])
+// console.log(y.__prot__);
+// console.log(y.length);
+// console.log(y.toUpperCase());
+// console.log(y.charAt(2));
+// console.log(y.indexOf('h'));
+// const newstring = y.slice(0,4);
+// console.log(newstring);
+// const anotherstring= y.slice(-4,2)
+// console.log(anotherstring);
+
+const a= "    yash     yash2        "
+console.log(a.trimEnd());
+console.log(a.replace("a","@"));// doesnot replace all a only the first one
+console.log(a.includes('ash'));
+console.log(y.split('-'));
+
+
+
+
+
+
